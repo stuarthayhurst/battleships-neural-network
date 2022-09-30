06e100fc057115955d2970f6ba971a82d170c4fb
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2022-09-30 12:25:36 +0100

    Implemented Graph class as a layered graph

--- structures.py
+++ structures.py
@@ -21,56 +21,73 @@ class Node:
   def __str__(self):
     return str(f"{self.name}: {[weight for weight in self.connectedNodesDict]}")
 
-class Graph:
+class Layer:
   def __init__(self):
-    self.nodesDict = {}
+    self.nodeDict = {}
     self.nodeCount = 0
-
-  def getNode(self, name):
-    if name in self.nodesDict.keys():
-      return self.nodesDict[name]
-    return None
- 
-  def getNodeList(self):
-    return list(self.nodesDict.keys())
 
   def addNode(self, name = None):
     #If name isn't specified, choose a free number
     if name == None:
       name = self.nodeCount
-      while name in self.nodesDict.keys():
+      while name in self.nodeDict.keys():
         name += 1
 
     #Return if the node already exists
-    if name in self.nodesDict.keys():
+    if name in self.nodeDict.keys():
       return None
 
     #Add a new node to the dicttionary
-    self.nodesDict[name] = Node(name)
+    self.nodeDict[name] = Node(name)
     self.nodeCount += 1
 
     #Return the node's name
     return name
 
-  def addEdge(self, head, tail, weight = 0, directional = False):
-    #Check head node exists
-    if head not in self.nodesDict.keys():
-      return None
+  def getNode(self, name):
+    if name in self.nodeDict.keys():
+      return self.nodeDict[name]
+    return None
 
-    #Check tail node exists
-    if tail not in self.nodesDict.keys():
-      return None
-
-    #Create the connections
-    self.nodesDict[head].addConnection(tail, weight)
-    if not directional:
-      self.nodesDict[tail].addConnection(head, weight)
-
-    #Return the connection's weight
-    return weight
+  def getNodeList(self):
+    return list(self.nodeDict.keys())
 
   def __iter__(self):
-    return iter(self.nodesDict.values())
+    return iter(self.nodeDict.values())
+
+class Graph:
+  def __init__(self):
+    self.layerDict = {}
+    self.layerCount = 0
+
+  def addLayer(self, name = None):
+    #If name isn't specified, choose a free number
+    if name == None:
+      name = self.layerCount
+      while name in self.layerDict.keys():
+        name += 1
+
+    #Return if the node already exists
+    if name in self.layerDict.keys():
+      return None
+
+    #Add a new node to the dicttionary
+    self.layerDict[name] = Layer()
+    self.layerCount += 1
+
+    #Return the node's name
+    return name
+
+  def getLayer(self, i):
+    if i in self.layerDict.keys():
+      return self.layerDict[i]
+    return None
+
+  def getLayerList(self):
+    return list(self.layerDict.keys())
+
+  def __iter__(self):
+    return iter(self.layerDict.values())
 
   def __contains__(self, name):
-    return name in self.nodesDict.keys()
+    return name in self.layerDict.keys()
