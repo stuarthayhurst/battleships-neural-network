bf782533e2aadfc98dff3dc909b6ee0c7a16f771
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-10-25 12:01:58 +0100

    Cleaned up main.py

--- main.py
+++ main.py
@@ -6,17 +6,11 @@ import dataset
 def getSeconds():
   return time.time_ns() / 1000000000
 
-startTime = getSeconds()
-
-#Define network parameters
-hiddenLayerCount, nodeCount, inputDimensions = 3, 10, 7
-networkSize = (((inputDimensions ** 2) * nodeCount) * 2) + (nodeCount ** 2) * (hiddenLayerCount - 1)
-
-#Attempt to load existing weights from file
-loadWeights, weightsValid, weights = True, False, []
-weightFile = "weights.dmp"
-try:
-  if loadWeights:
+def getWeights(weightFile, networkSize):
+  #Attempt to load existing weights from file
+  weightsValid = False
+  weights = []
+  try:
     with open(weightFile) as file:
       weights = file.readlines()
 
@@ -24,20 +18,33 @@ try:
       weights[i] = float(weights[i].strip("\n"))
 
     weightsValid = (len(weights) == networkSize)
-except:
-  print(f"Couldn't open {weightFile}")
+  except:
+    print(f"Couldn't open {weightFile}")
 
-if not weightsValid:
-  print("Failed to load existing model, using new empty weights\n")
-  weights = [0 for i in range(0, networkSize)]
-else:
-  print("Loaded existing model\n")
+  #Load empty weights if file couldn't be used
+  if not weightsValid:
+    print("Failed to load existing model, using new empty weights")
+    weights = [0 for i in range(0, networkSize)]
+  else:
+    print("Loaded existing model")
+
+  return weights
+
+startTime = getSeconds()
+
+#Define network parameters
+hiddenLayerCount, nodeCount, inputDimensions = 3, 10, 7
+networkSize = (((inputDimensions ** 2) * nodeCount) * 2) + (nodeCount ** 2) * (hiddenLayerCount - 1)
+
+#Load weights in from file, or generate default
+weightFile = "weights.dmp"
+weights = getWeights(weightFile, networkSize)
 
 #Create model and load weights
 exampleNetwork = model.Network(hiddenLayerCount, nodeCount, 1, inputDimensions ** 2)
 exampleNetwork.loadWeights(weights)
 
-print(f"Loaded network in {round(getSeconds() - startTime, 4)}s")
+print(f"\nLoaded network in {round(getSeconds() - startTime, 4)}s")
 startTime = getSeconds()
 
 #Define dataset parameters and generate data
@@ -55,25 +62,15 @@ if not exampleNetwork.loadDataset(generatedData):
 print(f"Loaded dataset in {round(getSeconds() - startTime, 4)}s")
 startTime = getSeconds()
 
-print()
-print(f"User board: {generatedData[0][0]}")
-print(f"Board: {generatedData[0][1]}")
-print(exampleNetwork.sampleData(generatedData[0][0]))
-print()
-
+sampleData = exampleNetwork.sampleData(generatedData[0][0])
 print(f"Ran network in {round(getSeconds() - startTime, 4)}s")
+
+print(f"\nUser board: {generatedData[0][0]}")
+print(f"Board: {generatedData[0][1]}")
+print(f"Result: {sampleData}\n")
 
 #Save model weights to file
 trainedWeights = exampleNetwork.dumpNetwork()
 with open(weightFile, "w") as file:
   for i in range(networkSize):
     file.write(f"{trainedWeights[i]}\n")
-
-"""
-Todo:
- - Generate n boards, and use for e epochs
- - Process returned results
- - Network training / improvement
-   - Potentially add more activation layers
-   - Add training / loss metrics
-"""
