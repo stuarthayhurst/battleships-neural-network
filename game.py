bf183c20a05c2cef49b40233c3fa65a83488e3ca
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-11-01 13:26:00 +0000

    Run multiple samples to evaluate network battleships performance

--- game.py
+++ game.py
@@ -39,66 +39,50 @@ weights = getWeights(weightFile, networkSize)
 exampleNetwork = model.Network(inputDimensions ** 2)
 exampleNetwork.loadWeights(weights)
 
-#Define dataset parameters and generate data
-datasetSize, boardDimensions = 1, 7
-maxHits = 0
-generatedData = dataset.buildDataset(datasetSize, boardDimensions, maxHits)
-
-
-grid = generatedData[0][0]
-ships = generatedData[0][1]
-
-print(grid)
-print(ships)
-
 def checkGrid(ships):
   if 1 in ships:
     return False
   return True
 
-totalGuesses = 0
-randomGuesses = 0
-while not checkGrid(ships):
-  totalGuesses += 1
-  guesses = exampleNetwork.sampleData(grid)
+sampleSize = 1000
+requiredGuesses = 0
+for sample in range(sampleSize):
+  #Define dataset parameters and generate data
+  datasetSize, boardDimensions = 1, 7
+  maxHits = 0
+  generatedData = dataset.buildDataset(datasetSize, boardDimensions, maxHits)
 
-  maxGuess = 0
-  maxGuessIndex = -1
-  for i in range(len(guesses)):
-    if guesses[i] >= maxGuess:
-      if grid[i] == 0:
-        print("setting to" + str(maxGuessIndex))
-        maxGuess = guesses[i]
-        maxGuessIndex = i
-  guess = maxGuessIndex
+  grid = generatedData[0][0]
+  ships = generatedData[0][1]
 
-  if guess == -1:
-    print("No guess")
-    randomGuesses += 1
-    guess = random.randint(0, len(grid) - 1)
-    while grid[guess] != 0:
+  totalGuesses = 0
+  randomGuesses = 0
+  while not checkGrid(ships):
+    totalGuesses += 1
+    guesses = exampleNetwork.sampleData(grid)
+
+    maxGuess = 0
+    maxGuessIndex = -1
+    for i in range(len(guesses)):
+      if guesses[i] >= maxGuess:
+        if grid[i] == 0:
+          maxGuess = guesses[i]
+          maxGuessIndex = i
+    guess = maxGuessIndex
+
+    if guess == -1:
+      randomGuesses += 1
       guess = random.randint(0, len(grid) - 1)
+      while grid[guess] != 0:
+        guess = random.randint(0, len(grid) - 1)
 
-  print(guess)
+    if ships[guess] == 1:
+      grid[guess] = 1
+      ships[guess] = 0
+    else:
+      grid[guess] = -1
 
-  if ships[guess] == 1:
-    grid[guess] = 1
-    ships[guess] = 0
-    print("Hit")
-  else:
-    grid[guess] = -1
-    print("Miss")
+  print(f"Random guesses: {randomGuesses} / {totalGuesses}")
+  requiredGuesses += totalGuesses
 
-  for y in range(boardDimensions):
-    for x in range(boardDimensions):
-      tile = grid[(y * boardDimensions) + x]
-      if tile == -1:
-        print("M", end = "")
-      elif tile == 1:
-        print("X", end = "")
-      else:
-        print("0", end = "")
-    print()
-
-print(totalGuesses)
-print(f"Random guesses: {randomGuesses} / {totalGuesses}")
+print(f"Average guesses: {requiredGuesses / sampleSize}")
